f7bc2c2dd8e053300b50854e3c42e7dd8b5416ce
Damien <damien@kaliDamien.com>
2026-04-16 21:13:44 +0200

    Update file conf and function check_ports

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -5,7 +5,7 @@ then
 	exit 1
 fi
 source "$FILE"
-if [ -z "${SERVICES+x}" ] || [ -z "${FILES_TO_WATCH+x}" ];
+if [ -z "${SERVICES+x}" ] || [ -z "${FILES_TO_WATCH+x}" ] || [ -z "${ALLOWED_PORTS+x}" ];
 then
 	exit 1
 fi
@@ -30,13 +30,21 @@ check_services() {
 check_integrity() {
 	for file in "${FILES_TO_WATCH[@]}";
 	do
-		if [ "$(md5sum "$file")" == "$(md5sum /var/backups/sentinel/"$(basename "$file")".gold)" ];
+		current_hash=$(md5sum "$file" | awk '{print $1}')
+		gold_hash=$(md5sum /var/backups/sentinel/"$(basename "$file")".gold | awk '{print $1}')
+		if [ "$current_hash" == "$gold_hash" ];
 		then
-			echo "OK: $file integrity verified"
+			echo "OK: $(basename $file) integrity verified"
 		else
 			cp /var/backups/sentinel/"$(basename "$file")".gold "$file"
-			echo "FIXED: Restored $file"
+			echo "FIXED: Restored $(basename $file)"
 		fi
+	done
+}
+
+check_ports() {
+	ss -tlnp | tail -n +2 | while read -r line;
+	do
 	done
 }
 
