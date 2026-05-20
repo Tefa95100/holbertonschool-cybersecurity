cff176009b81e6874414d82a9a716e200a47b437
Damien <damien@kaliDamien.com>
2026-05-20 12:56:02 +0200

    Correction of conversion is ok

--- network_security/2x00_network_fundamentals/3-ip2bin.sh
+++ network_security/2x00_network_fundamentals/3-ip2bin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-[[ $1 =~ ^((25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})$ ]] && printf "%08d\n" "$(echo "obase=2;$1" | bc)" || echo "Seul une adresse ip valide est accepté."
+[[ $1 =~ ^((25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})$ ]] && IFS=. read a b c d <<< "$1" && printf "%08d.%08d.%08d.%08d\n" "$(echo "obase=2;$a" | bc)" "$(echo "obase=2;$b" | bc)" "$(echo "obase=2;$c" | bc)" "$(echo "obase=2;$d" | bc)" || echo "Seul une adresse ip valide est accepté."
