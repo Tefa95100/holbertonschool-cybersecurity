b41d57314a77f80a7938bfca41585cebed73e2f0
Damien <damien@kaliDamien.com>
2026-05-21 14:05:29 +0200

    Remove eof in printf

--- network_security/2x00_network_fundamentals/6-broadcast.sh
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS=. read ip1 ip2 ip3 ip4 <<< "$1"; IFS=. read mask1 mask2 mask3 mask4 <<< "$2"; printf "%d.%d.%d.%d\n" $(( (ip1 & mask1) | (mask1 ^ 255) )) $(( (ip2 & mask2) | (mask2 ^ 255) )) $(( (ip3 & mask3) | (mask3 ^ 255) )) $(( (ip4 & mask4) | (mask4 ^ 255) ))
+IFS=. read ip1 ip2 ip3 ip4 <<< "$1"; IFS=. read mask1 mask2 mask3 mask4 <<< "$2"; printf "%d.%d.%d.%d" $(( (ip1 & mask1) | (mask1 ^ 255) )) $(( (ip2 & mask2) | (mask2 ^ 255) )) $(( (ip3 & mask3) | (mask3 ^ 255) )) $(( (ip4 & mask4) | (mask4 ^ 255) ))
